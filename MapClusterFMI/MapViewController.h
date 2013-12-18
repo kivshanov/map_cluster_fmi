e8904e63166514083467f90c1824cb04ba1ed9cb
Pavlina Gatova <p.gatova@gmail.com>
2013-12-18 13:32:18 +0200

    add MapViewController

---
+++ MapClusterFMI/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  MapClusterFMI
+//
+//  Created by Pavlina Gatova on 12/18/13.
+//  Copyright (c) 2013 FMI Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController <MKMapViewDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ MapClusterFMI/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  MapClusterFMI
+//
+//  Created by Pavlina Gatova on 12/18/13.
+//  Copyright (c) 2013 FMI Project. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
