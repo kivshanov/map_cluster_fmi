4ef8eabb5944c9a8035cfb2e686faa594d8bc472
kivshanov <nkivshanov@Nikolays-Mac-mini.local>
2013-12-28 14:43:23 +0200

    Swipe Action animation + 3 choices and showing the map

--- MapClusterFMI/MenuViewController.h
+++ MapClusterFMI/MenuViewController.h
@@ -12,5 +12,11 @@
 
 @property (nonatomic , weak) IBOutlet UIImageView *logoImg;
 @property (nonatomic , weak) IBOutlet UIImageView *swipeImg;
+@property (nonatomic , weak) IBOutlet UIButton *button1;
+@property (nonatomic , weak) IBOutlet UIButton *button2;
+@property (nonatomic , weak) IBOutlet UIButton *button3;
+
+- (IBAction)swipeAction:(UISwipeGestureRecognizer *)sender;
+- (IBAction)menuTapped:(UIButton *)sender;
 
 @end
--- MapClusterFMI/MenuViewController.m
+++ MapClusterFMI/MenuViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MenuViewController.h"
+#import "ObjectChoiceViewController.h"
+#import "MapViewController.h"
 
 @interface MenuViewController ()
 
@@ -14,9 +16,44 @@
 
 @implementation MenuViewController
 
+- (IBAction)menuTapped:(UIButton *)sender {
+    switch (sender.tag) {
+        case 0:
+            NSLog(@"parks");
+            break;
+        case 1:
+            NSLog(@"restaurants");
+            break;
+        case 2:
+            NSLog(@"cafes");
+            break;
+            
+        default:
+            break;
+    }
+    
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         self.button1.frame = CGRectMake(-320, self.button1.frame.origin.y, self.button1.frame.size.width, self.button1.frame.size.height);
+                         self.button2.frame = CGRectMake(-320, self.button2.frame.origin.y, self.button2.frame.size.width, self.button2.frame.size.height);
+                         self.button3.frame = CGRectMake(-320, self.button3.frame.origin.y, self.button3.frame.size.width, self.button3.frame.size.height);
+                     }
+                     completion:^(BOOL finished) {
+                         
+                         MapViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"map"];
+                         [self presentViewController:vc animated:NO completion:^{
+                             //
+                         }];
+                         
+                     }];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UISwipeGestureRecognizer *gr = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeAction:)];
+    gr.direction = UISwipeGestureRecognizerDirectionLeft;
+    [self.view addGestureRecognizer:gr];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -41,8 +78,38 @@
     
 }
 
+- (void)showObjectOption {
+    [UIView animateWithDuration:0.8
+                     animations:^{
+                         self.button1.alpha = 1.0;
+                         self.button2.alpha = 1.0;
+                         self.button3.alpha = 1.0;
+                     }
+                     completion:^(BOOL finished) {
+                         
+                     }];
+
+}
+
 - (void)animationDidStop:(CAAnimation *)theAnimation finished:(BOOL)flag {
     self.swipeImg.alpha = 0.0;
+}
+
+
+- (IBAction)swipeAction:(UISwipeGestureRecognizer *)sender {
+    NSLog(@"swipe");
+    if(sender.direction == UISwipeGestureRecognizerDirectionLeft) {
+        if(!self.logoImg.hidden) {
+            [UIView animateWithDuration:0.5
+                             animations:^{
+                                 self.logoImg.frame = CGRectMake(-320, self.logoImg.frame.origin.y, self.logoImg.frame.size.width, self.logoImg.frame.size.height);
+                             }
+                             completion:^(BOOL finished) {
+                                 self.logoImg.hidden = YES;
+                                 [self showObjectOption];
+                             }];
+        }
+    }
 }
 
 @end
